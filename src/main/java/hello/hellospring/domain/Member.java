227aa6b20792e691930491a9b0dba7f255c1ff7a
cjsrhd94 <cjsrhd94@gmail.com>
2021-10-13 18:55:02 +0900

    feat: JDBC 및 JPA를 통한 DB접근 클래스 개발

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)     //DB에서 자동으로 아이디를 부여하는 전략.
     private Long id;
+
     private String name;
 
     public Long getId() {
